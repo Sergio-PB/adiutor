a4c386c22fecab46c73939a5423a7a89858bbcfb
Sergio <sergiopb1998@gmail.com>
2018-08-12 17:26:17 -0300

    Add index.html page with form Add main.css base style Add login.html page without content Create and add Terapeutas and Pacientes models Create superuser

--- adiutor/pacientes/models.py
+++ adiutor/pacientes/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+class terapeutas(models.Model):
+    Nome = models.CharField(max_length=20)
+    # CRP 12/ _ _ _ _ _
+    Crp = models.IntegerField()
+    Senha = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.Nome
+
+    class Meta:
+        verbose_name = "Terapeutas"
+        verbose_name_plural = "Terapeutas"
+
+class pacientes(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    Nome = models.CharField(max_length=200)
+    Responsavel = models.CharField(max_length=255, blank=True)
+    Parentesco = models.CharField(max_length=200, blank=True)
+    Nascimento = models.DateField()
+    SEX = (
+        ('MASCULINO', 'Masculino'),
+        ('FEMININO', 'Feminino'),
+    )
+    Sexo = models.CharField(max_length=2, choices=SEX)
+    Cadastro = models.DateField()
+    Inicio = models.DateField()
+    Termino = models.DateField(blank=True)
+    Agendamento = models.IntegerField()
+    CONVENIOS = (
+        ('UNIMED', 'UNIMED'),
+        ('AGEMED', 'AGEMED'),
+        ('ABEPOM', 'ABEPOM'),
+        ('PARTICULAR', 'PARTICULAR'),
+        ('LOCUS', 'LOCUS'),
+        ('ELASE', 'ELASE'),
+        ('CORTESIA', 'CORTESIA'),
+    )
+    Convenio = models.CharField(max_length=20, choices=CONVENIOS)
+    MOD = (
+        ('INDIVIDUAL', 'Individual'),
+        ('CASAL', 'Casal'),
+        ('FAMILIA', 'Familia'),
+        ('GRUPO', 'Grupo'),
+        ('AVALIACAO', 'Avaliação'),
+    )
+    Modalidade = models.CharField(max_length=20, choices=MOD)
+    Celular = models.IntegerField(blank=True)
+    Whats = models.IntegerField( blank=True)
+    Fixo = models.IntegerField(blank=True)
+    Email = models.EmailField(max_length=40)
+    CIVIL = (
+        ('SOLTEIRO', 'Solteiro'),
+        ('CASADO', 'Casado'),
+        ('DIVORCIADO', 'Divorciado'),
+        ('VIUVO', 'Viúvo'),
+        ('UNIAO-ESTAVEL', 'União Estável'),
+    )
+    EstadoCivil = models.CharField(max_length=20, choices=CIVIL)
+    Profissao = models.CharField(max_length=100)
+    Rua = models.CharField(max_length=255)
+    Numero = models.CharField(max_length=255)
+    Complemento = models.CharField(max_length=255)
+    Bairro = models.CharField(max_length=255)
+    Cidade = models.CharField(max_length=255)
+    Estado = models.CharField(max_length=2)
+    Observacao = models.TextField()
+
+    def __str__(self):
+        return self.Nome
+
+    class Meta:
+        verbose_name = "Pacientes"
+        verbose_name_plural = "Pacientes"
--- adiutor/pacientes/views.py
+++ adiutor/pacientes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'pacientes/index.html')
+
+def login(request):
+    name = request.GET.get('nome')
+    crp = request.GET.get('crp')
+    password = request.GET.get('senha')
+    context = {'nome':nome, 'crp':crp,'senha':senha}
+
+    return render(request, 'pacientes/login.html', context)
